0369f94bc737ea5b7040587eb1ce8c1ec9871b2a
jazid Sánchez <jazid.sanchez@utp.ac.pa>
2026-08-28 08:39:43 -0500

    Completar formularioHTML y pagina2 con comentarios segun la rubrica

--- formularioHTML.php
+++ formularioHTML.php
@@ -1,36 +1,23 @@
-<!DOCTYPE html>
 <html>
 <head>
     <title>Formulario de entrada del dato</title>
 </head>
 <body>
 
-    <form method="post" action="">
-        Ingrese su nombre:<br>
-        <input type="text" name="nombre" id="nombre"><br><br>
-
-        Ingrese su edad:<br>
-        <input type="text" name="edad" id="edad"><br><br>
-
+    <!-- PUNTO 5 DE LA RÚBRICA: Estructura de formulario HTML con etiquetas form e input -->
+    <form method="post" action="pagina2.php">
+        Ingrese su nombre:
+        <input type="text" name="nombre" id="nombre">
+        <br>
+        <br>
+        Ingrese su Edad:
+        <input type="text" name="edad" id="edad">
+        <br>
+        <br>
         <input type="submit" value="confirmar">
     </form>
 
-    <hr>
-
-<?php
-if (isset($_POST['nombre']) && isset($_POST['edad'])) {
-    $Nombre = $_POST['nombre'];
-    $Edad = intval($_POST['edad']);
-
-    echo "El nombre es: " . $Nombre . "<br>";
-
-    if ($Edad >= 18) {
-        echo "Usted puede votar en las próximas elecciones 2028";
-    } else {
-        echo "Usted no es mayor de edad";
-    }
-}
-?>
-
+</body>
+</html>
 </body>
 </html>
--- pagina2.php
+++ pagina2.php
@@ -1,17 +1,21 @@
 <?php
 
-if (isset($_POST['nombre']) && isset($_POST['edad'])) {
-    $Nombre = $_POST['nombre'];
-    $Edad = intval($_POST['edad']);
+// PUNTO 9 DE LA RÚBRICA: Comentarios explicativos sobre las variables y condicionales
 
-    echo "El nombre es: " . $Nombre . "<br>";
+// Captura del nombre mediante la superglobal $_REQUEST
+$Nombre = $_REQUEST['nombre'];
+echo "El nombre es: " . $Nombre . "<br>";
 
-    if ($Edad >= 18) {
-        echo "Usted puede votar en las próximas elecciones 2028";
-    } else {
-        echo "Usted no es mayor de edad";
-    }
+// PUNTO 4 DE LA RÚBRICA: Captura de datos enviados mediante el método POST
+$Edad = $_POST['edad'];
+
+// Evaluación condicional para determinar si es mayor de edad
+if (isset($Edad) and $Edad >= 18) {
+    // Acciones ejecutadas si la edad es igual o mayor a 18
+    echo "usted puede votar en las próximas elecciones 2028";
 } else {
-    echo "Por favor, llene el formulario primero.";
+    // Acciones si no cumple con la condición
+    echo "Usted no es mayor de edad";
 }
+
 ?>
